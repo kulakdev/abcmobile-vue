8347b09c04cf89a0a4f7156be65d982b4973b6e3
jke <constantinekulak@proton.me>
2023-04-17 18:04:14 +0300

    removed the default components

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,7 @@ import IphoneImage from './components/IphoneImage.vue'
 
 <template>
   <IphoneImage />
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <ButtonComponent />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <ButtonComponent />
 </template>
 
 <style scoped>
