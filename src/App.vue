dd7e0ee66d166b9ec41e7feb2654e215cafe3004
jke <constantinekulak@proton.me>
2023-04-17 22:09:53 +0300

    created a phone to display card in

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import IphoneImage from './components/IphoneImage.vue'
 <template>
   <div class="wrapper">
     <ButtonComponent text="ДАЛЕЕ" />
-    <div class="card"></div>
+    <div class="phone">
+      <div class="card"></div>
+    </div>
   </div>
 </template>
 
@@ -17,14 +19,36 @@ import IphoneImage from './components/IphoneImage.vue'
   height: 100vh;
   background-color: darkcyan;
 }
-.card {
-  height: 569px;
-  width: 320px;
-  background-color: black;
+
+.phone {
+  width: 372px;
+  height: 753px;
 
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  background-color: blue;
+}
+.card {
+  left: 27px;
+  top: 93px;
+  height: 569px;
+  width: 320px;
+  background-color: black;
+  overflow-y: scroll;
+}
+.card::-webkit-scrollbar {
+  width: 1em;
+}
+
+.card::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+.card::-webkit-scrollbar-thumb {
+  background-color: darkgrey;
+  outline: 1px solid slategrey;
 }
 </style>
