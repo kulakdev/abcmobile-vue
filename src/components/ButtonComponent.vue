40d2986ca53dd2eecc26e0a267435cbee3ca7405
jke <constantinekulak@proton.me>
2023-04-17 17:55:56 +0300

    init repo, create button component, bg image component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="buttonStyle" role="button">ДАЛЕЕ</button>
+</template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300;700&display=swap');
+.buttonStyle {
+  position: absolute;
+  width: 189px;
+  height: 41px;
+  background: radial-gradient(50% 50% at 50% 50%, #ffc700 0%, #ffc700 100%);
+  border-radius: 20px;
+  border-style: none;
+
+  font-family: 'Merriweather';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 18px;
+  /* identical to box height */
+
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+
+  color: #0d0c11;
+}
+.buttonStyle:active {
+  background: #dadada;
+  color: #8e8e8e;
+  box-shadow: inset 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+}
+</style>
---
+++ src/components/IphoneImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="image-container">
+    <img :src="imageUrl" class="centered-image" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CenteredImage',
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.centered-image {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
