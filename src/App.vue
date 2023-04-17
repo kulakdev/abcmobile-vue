d8f053fb0c5ce3edc14ad9d861423d6fe3f4c340
jke <constantinekulak@proton.me>
2023-04-17 18:04:35 +0300

    cleaned up the dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import ButtonComponent from './components/ButtonComponent.vue'
 import IphoneImage from './components/IphoneImage.vue'
 </script>
