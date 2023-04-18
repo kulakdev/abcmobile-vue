c477958bd913461ba2e853fc0da9a0840888bcb0
jke <constantinekulak@proton.me>
2023-04-18 18:12:39 +0300

    fixed unused MenuIcon component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,16 @@
 <script lang="ts">
+import MenuIcon from './icons/MenuIcon.vue'
+
 export default {
-  name: 'NavBar'
+  name: 'NavBar',
+  components: { MenuIcon }
 }
 </script>
 
 <template>
   <div class="navbar">
     <a class="button">
-      <div class="icon"></div>
-      <div class="icon"></div>
-      <div class="icon"></div>
+      <MenuIcon />
     </a>
   </div>
 </template>
---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="icon"></div>
+  <div class="icon"></div>
+  <div class="icon"></div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style scoped>
+.icon {
+  width: 24px;
+  height: 1mm;
+  background-color: #696969;
+}
+</style>
