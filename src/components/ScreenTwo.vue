83785db9e8db500c34a092c12a7004a224d9d17a
jke <constantinekulak@proton.me>
2023-04-18 22:50:08 +0300

    added image, button

--- src/components/ScreenTwo.vue
+++ src/components/ScreenTwo.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="secondScreen">
-    <p>
+    <p class="text">
       Также по результатам теста <span class="bold">вы получите</span> подробные
       <span class="bold">советы</span> по определению наиболее перспективной
       <span class="bold">для вашего типа интеллекта сферы деятельности</span>, которая принесет вам
       скорейший финансовый результат.
     </p>
+    <img class="imageElement" src="src/assets/brains.webp" />
+    <ButtonComponent text="ПРОЙТИ ТЕСТ" />
   </div>
 </template>
 
 <script lang="ts">
+import ButtonComponent from './ButtonComponent.vue'
+
 export default {
-  name: 'ScreenTwo'
+  name: 'ScreenTwo',
+  components: { ButtonComponent }
 }
 </script>
 
 <style scoped>
 .secondScreen {
   width: 100%;
-  height: 700px;
+  height: max-content;
   padding: 38px 11px;
   background-color: #fff;
   color: #272727;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   font-family: 'PT Serif', serif;
   font-style: normal;
@@ -32,6 +41,13 @@ export default {
   letter-spacing: 0.05em;
 }
 
+.text {
+  width: inherit;
+}
+.imageElement {
+  margin-left: 33px;
+  margin-right: 33px;
+}
 .bold {
   font-weight: bold;
   text-transform: uppercase;
