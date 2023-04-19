9a32c41a270ccc86ce7ecd019a3ebccbe4a50bf2
jke <constantinekulak@proton.me>
2023-04-19 20:07:36 +0300

    created the menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,59 @@
 <script lang="ts">
 import MenuIcon from './icons/MenuIcon.vue'
 
-export default {
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
   name: 'NavBar',
-  components: { MenuIcon }
-}
+  components: { MenuIcon },
+  setup() {
+    const myBoolean = ref(true)
+
+    function toggleComponent() {
+      myBoolean.value = !myBoolean.value
+    }
+
+    return {
+      myBoolean,
+      toggleComponent
+    }
+  }
+})
 </script>
 
 <template>
   <div class="navbar">
-    <a class="button">
+    <a class="button" @click="toggleComponent">
       <MenuIcon />
     </a>
+    <p style="color: aqua; font-size: medium">{{ myBoolean }}</p>
+
+    <div
+      v-if="!myBoolean"
+      style="
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        height: 100vh;
+        width: 100%;
+        background-color: brown;
+      "
+    >
+      <a
+        style="
+          position: relative;
+          display: block;
+          width: 100px;
+          height: 100px;
+          min-width: fit-content;
+          background-color: tomato;
+        "
+        @click="toggleComponent"
+      >
+        <MenuIcon />
+      </a>
+    </div>
   </div>
 </template>
 
