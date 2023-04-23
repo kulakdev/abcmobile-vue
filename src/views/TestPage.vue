f5abe3434267f3bf1468a06c2661922f0445ead4
jke <constantinekulak@proton.me>
2023-04-23 18:42:18 +0300

    hooked up radio button's styles to the same state as bg

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -2,14 +2,36 @@
   <div class="testPage">
     <div v-if="selectedPage" class="question">{{ selectedPage.title }}</div>
 
-    <div v-if="selectedPage.image">
-      <img style="width: 100px" :src="selectedPage.image" />
+    <div v-if="selectedPage.image" class="imageContainer">
+      <img :src="selectedPage.image" />
     </div>
 
-    <div v-if="selectedPage.list" style="width: 100%">
-      <div v-for="item in selectedPage.list" v-bind:key="item" class="checkbox">
-        <input name="answer" type="radio" id="item" :value="item" @click="setSelectedCell(item)" />
-        <label for="answer" class="textWhite">{{ item }}</label>
+    <div v-if="selectedPage.list" class="list" style="width: 100%">
+      <div v-for="item in selectedPage.list" v-bind:key="item">
+        <div
+          name="answer"
+          :value="item"
+          class="checkbox textWhite"
+          :class="{ checkboxActive: state.selectedCell == item }"
+          @click="setSelectedCell(item)"
+        >
+          <div class="round" tabindex="1" :class="{ roundActive: state.selectedCell == item }" />
+          <div id="answer" :class="{ checkboxActive: state.selectedCell == item }">{{ item }}</div>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="selectedPage.numbers" class="numbers">
+      <div class="numbdersBr" />
+      <div
+        tabindex="0"
+        v-for="(cell, index) in selectedPage.numbers"
+        class="numbercell"
+        :key="index"
+        :style="`background-color:` + cell"
+        @click="setSelectedCell(index)"
+      >
+        {{ cell }}
       </div>
     </div>
 
@@ -24,7 +46,9 @@
       ></div>
     </div>
 
-    <ButtonComponent text="Next" @click="incrementIndex" :disabled="!state.selectedCell" />
+    <div class="buttonComponent">
+      <ButtonComponent text="Next" @click="incrementIndex" :disabled="!state.selectedCell" />
+    </div>
   </div>
 </template>
 
@@ -35,7 +59,7 @@ import { reactive, computed } from 'vue'
 export default {
   data() {
     const state = reactive({
-      currentIndex: 5,
+      currentIndex: 0,
       answers: [],
       selectedCell: '',
       people: [
@@ -44,7 +68,7 @@ export default {
           title: 'Укажите ваш возраст',
           list: ['До 18', 'От 18 до 28', 'От 29 до 35', 'От 36'],
           image: 'src/assets/brain.webp'
-        },
+        },s
         { title: 'Выбери лишнее:', list: ['Дом', 'Шалаш', 'Бунгало', 'Скамейка', 'Хижина'] },
         {
           title: 'Продолжите числовой ряд: 18  20  24  32',
@@ -60,9 +84,24 @@ export default {
         },
         {
           title: 'Выберите правильную фигуру из четырёх пронумерованных.',
+          image: 'src/assets/dudes.webp',
           numbers: ['1', '2', '3', '4']
+        },
+        {
+          title: 'Вам привычнее и важнее:',
+          list: [
+            'Наслаждаться каждой минутой проведенного времени',
+            'Быть устремленными мыслями в будущее',
+            'Учитывать в ежедневной практике прошлый опыт'
+          ]
         }
       ]
+    })
+
+    const answerStyle = computed(() => {
+      return {
+        backgroundColor: state.selectedCell ? '#00FF00' : '#FFF'
+      }
     })
 
     const incrementIndex = () => {
@@ -90,6 +129,7 @@ export default {
       selectedPage,
       incrementIndex,
       setSelectedCell,
+      answerStyle,
       selectedCell: state.selectedCell
     }
   },
@@ -101,11 +141,20 @@ export default {
 .testPage {
   padding: 46px 10px 0px;
   width: 100%;
+  height: 570px;
   font-family: 'PT Serif', serif;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
 }
+
+@media (min-width: 576px) {
+  .testpage {
+    height: 570px;
+  }
+}
+
 .question {
   font-size: 20;
 }
@@ -130,6 +179,33 @@ export default {
   border: 6px solid yellow;
 }
 
+.numbers {
+  color: black;
+  display: flex;
+  width: 100%;
+}
+
+.numbersBr {
+  width: 320px;
+  height: 4px;
+  background: #f2f3f3;
+  opacity: 0.15;
+}
+
+.numbers > img {
+  min-width: 185px;
+}
+
+.numbercell {
+  background-color: #fff;
+  width: 41px;
+  height: 41px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 15px;
+}
+
 .invisible-radio {
   border: 6px solid yellow;
   /* appearance: none;
@@ -148,13 +224,79 @@ export default {
   border: 6px solid green;
 }
 
+.imageContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.imageContainer > img {
+  width: 58%;
+}
+
+.list {
+  font-family: 'PT Serif';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+
+  letter-spacing: 0.05em;
+  text-transform: capitalize;
+
+  color: #ffffff;
+}
+
+.circlebutton
+.circlebuttonactive: {
+  background: #2950c2;
+  border: 1px solid #272727;
+}
+
+#answer {
+  width: 100%;
+  height: 100%;
+}
+
+.round {
+  width: 20px;
+  height: 20px;
+  background-color: transparent;
+  border-radius: 50%;
+  border: 1px solid white;
+  margin: 15px;
+}
+
+.roundActive {
+  border: 1px solid black;
+  background-color: #2950c2;
+}
+
 .checkbox {
-  height: 50px;
+  min-height: 50px;
+  height: max-content;
   width: 100%;
   padding: 0px;
   background: rgba(242, 243, 243, 0.15);
   margin: 4px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
   color: #fff;
+}
+
+.checkboxActive {
+  background-color: #ffc700;
+  color: black;
+}
+
+.checkbox:focus {
+  background-color: yellow;
+  color: black;
+}
+
+.buttonComponent {
+  margin-top: 23px;
+  margin-bottom: 23px;
 }
 </style>
